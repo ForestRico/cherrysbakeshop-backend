ac67cd4972d9762a68d4cb972be089bae9f62fc6
ForestRico <126733661+ForestRico@users.noreply.github.com>
2024-03-10 20:38:49 +1100

    backend changes

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI);
+        await mongoose.connect('mongodb+srv://bakeshopadmin:bakeshoppassword123@cluster0.qzdcnro.mongodb.net/BakeshopDB?retryWrites=true&w=majority&appName=Cluster0');
         // await mongoose.connect('mongodb://localhost:5000/BakeshopDB');
         console.log("Database connected successfully");
     } catch (err) {
